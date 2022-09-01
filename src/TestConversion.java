7baa4254446bcfa7a83bc1ef469c95af1ce771e5
Junior Sott <sottjr@gmail.com>
2022-09-02 01:53:04 +0200

    studys of numbers, float, int, double

--- src/TestConversion.java
+++ src/TestConversion.java
@@ -1,10 +1,17 @@
 
 public class TestConversion {
 	public static void main(String[] args) {
+		
+		float pontoFlutuante = 3.14f;
+		
 		double salary = 1270.5;
 		int value = (int) salary;
 		System.out.println(value);
 		
-		
+		double value1 = 0.2;
+        double value2 = 0.1;
+        double total = value1 + value2;
+
+        System.out.println(total);
 	}
 }
