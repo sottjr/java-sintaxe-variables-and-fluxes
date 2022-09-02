297c8926315683b0e79ee4800e7b0ed7188a87a2
Junior Sott <sottjr@gmail.com>
2022-09-02 03:04:00 +0200

    add first if

---
+++ src/TestConditional.java
@@ -0,0 +1,22 @@
+
+public class TestConditional {
+
+	public static void main(String[] args) {
+		System.out.println("testando condicionais");
+		int idade = 20;
+		int quantidadePessoas = 3;
+
+		if (idade >= 18) {
+			System.out.println("você tem mais de 18 anos");
+			System.out.println("Seja bem vindo");
+		} else {
+			if (quantidadePessoas >= 2) {
+				System.out.println("voce nao tem 18, mas pode entrar, pois está acompanhado");
+			} else {
+
+				System.out.println("infelizmente voce nao pode entrar");
+
+			}
+		}
+	}
+}
