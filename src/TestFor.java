8c4f4eef61e3eb0d968c3d940346a6f5a8a9fdcf
Junior Sott <sottjr@gmail.com>
2022-09-06 01:16:03 +0200

    test for

---
+++ src/TestFor.java
@@ -0,0 +1,11 @@
+
+public class TestFor {
+	public static void main(String[] args) {
+		
+		for (int count = 0; count <= 10; count++) {
+			int number = count;
+			System.out.println(number);
+			
+		}
+	}
+}
