53f5f99e12f73d2efab1dbb85bf467c2ce9ff0d7
Junior Sott <sottjr@gmail.com>
2022-09-06 01:08:43 +0200

    while java

---
+++ src/TestWhile.java
@@ -0,0 +1,12 @@
+import org.w3c.dom.css.Counter;
+
+public class TestWhile {
+	public static void main(String[] args) {
+		int counter = 1;
+		
+		 while (counter <= 10) {
+			 System.out.println(counter);
+			counter++;
+		}
+	}
+}
