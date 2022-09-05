0d990e950bbbf47f7929dffc1b7cf3f57fbdb8c2
Junior Sott <sottjr@gmail.com>
2022-09-06 01:32:02 +0200

    loop of repetition in for

---
+++ src/LoopOfRepetition.java
@@ -0,0 +1,17 @@
+import java.io.Console;
+
+public class LoopOfRepetition {
+
+	public static void main(String[] args) {
+		for (int multiply = 1; multiply <=10; multiply++) {
+			for (int number = 1; number <=10; number++) {
+				System.out.print(multiply * number);
+				System.out.print(" ");
+			}
+				
+				System.out.println();
+			
+		}
+	}
+
+}
